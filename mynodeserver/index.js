47522ed5e4bea5fc4468a059d809ed01a98bf7c3
aniketh113 <59177621+aniketh113@users.noreply.github.com>
2023-12-05 17:08:10 -0500

    Update index.js

--- mynodeserver/index.js
+++ mynodeserver/index.js
@@ -1,7 +1,23 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const server = http.createServer((req, res) => {
+const { MongoClient } = require('mongodb');
+
+const URI = 'mongodb+srv://yelgondareddy:Aniketh65@cluster0.2mnyrsu.mongodb.net/';
+
+async function connectToMongoDB() {
+  const client = new MongoClient(URI);
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db('destinations');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    
+  }
+}
+const server = http.createServer(async (req, res) => {
 
     if (req.url === '/') {
     
@@ -52,24 +68,27 @@ const server = http.createServer((req, res) => {
                                     
                                     if (err) throw err;
                                     res.setHeader("Access-Control-Allow-Origin", "*")
-                                    res.writeHead(200, { 'Content-Type': 'html' });
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
                                     res.end(content);
                         }
               );
      }
     else if (req.url==='/api')
     {
-        fs.readFile(
-            path.join(__dirname, 'public', 'db.json'),'utf-8',
-                    (err, content) => {
-                                    
-                                    if (err) throw err;
-                                    res.setHeader("Access-Control-Allow-Origin", "*")
-                                    // Please note the content-type here is application/json
-                                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                                    res.end(content);
-                        }
-              );
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    try {
+      const db = await connectToMongoDB();
+      const data = await db.collection('places').find({}).toArray();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      console.log(data)
+      res.end(JSON.stringify(data));
+    } catch (error) {
+      console.error('Error retrieving data:', error);
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'Internal Server Error' }));
+    }
     }
     else{
         res.end("<h1> 404 nothing is here</h1>");
